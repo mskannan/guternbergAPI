cb56192edab33f42ef0ac293a72e407ab48eed03
mskannan <senthkannan55@gmail.com>
2025-02-23 12:14:46 +0530

    Comments on Api function

--- books/views.py
+++ books/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.db.models import Q
 from rest_framework.decorators import api_view
 from drf_spectacular.utils import extend_schema, OpenApiParameter
-from .models import Book,BooksView
+from .models import Book, BooksView
 from itertools import zip_longest
 
 @extend_schema(
@@ -22,12 +22,15 @@ from itertools import zip_longest
     ],
     responses={200: "application/json"},
 )
-
 @api_view(['GET'])
-def book_list(request): 
+def book_list(request):  
+    """
+    View to retrieve a list of books with support for filtering, searching, and pagination.
+    """
     
-    limit = int(request.GET.get('limit', 25))
-    offset = int(request.GET.get('offset', 0))
+    # Get query parameters
+    limit = int(request.GET.get('limit', 25))  # Number of books per page
+    offset = int(request.GET.get('offset', 0))  # Starting index for pagination
     title_filter = request.GET.get('title', '')
     topic_filter = request.GET.get('topic', '')
     author_filter = request.GET.get('author', '')
@@ -35,76 +38,71 @@ def book_list(request):
     language_filter = request.GET.get('languages', '')
     book_id_filter = request.GET.get('id', '')
 
-    books = BooksView.objects.all()
+    books = BooksView.objects.all()  # Fetch all books from the view
 
-    # Filter by bookid
+    # Filtering by book ID
     if book_id_filter:
         book_ids = book_id_filter.split(',')
         books = books.filter(gut_book_id__in=book_ids)    
     
-    # Filter by Topic
+    # Filtering by Topic (subject or title match)
     if topic_filter:
         topic_filters = topic_filter.split(',')
         topicquery = Q()
         for topics in topic_filters:
-            topicquery |= Q(subjects__icontains=topics) 
+            topicquery |= Q(subjects__icontains=topics)  
             topicquery |= Q(booktitle__icontains=topics)  
-        
         books = books.filter(topicquery)
 
-    # Filter by Title
+    # Filtering by Title
     if title_filter:
         title_filters = title_filter.split(',')
         titlequery = Q()
         for titles in title_filters:
             titlequery |= Q(booktitle__icontains=titles)  
-        
         books = books.filter(titlequery)
 
-    
-    # Filter by author
+    # Filtering by Author
     if author_filter:
         author_filters = author_filter.split(',')
         authorquery = Q()
         for authors in author_filters:
             authorquery |= Q(author_name__icontains=authors)  
-        
         books = books.filter(authorquery)
 
-    # Filter by mime type
+    # Filtering by Mime Type
     if mime_filter:
         mime_filters = mime_filter.split(',')
         mimequery = Q()
         for mimes in mime_filters:
             mimequery |= Q(mime_type__icontains=mimes)  
-        
         books = books.filter(mimequery)
     
-    # Filter by language
+    # Filtering by Language
     if language_filter:
         language_filters = language_filter.split(',')
         langquery = Q()
         for lang in language_filters:
             langquery |= Q(languages__icontains=lang)  
-        
         books = books.filter(langquery)
 
-
+    # Pagination setup
     total_books = books.count()
-    paginator = Paginator(books, limit) 
-    page_number = (offset // limit) + 1
+    paginator = Paginator(books, limit)  # Paginate results
+    page_number = (offset // limit) + 1  # Determine current page number
 
     if total_books == 0:
-        return JsonResponse({"Message":"Please try someother filter or search options"})
+        return JsonResponse({"Message": "Please try some other filter or search options"})
 
     try:
         current_page = paginator.page(page_number)
     except:
         return JsonResponse({"error": "Invalid page number"}, status=400)
  
+    # Convert paginated books to JSON
     books_json = generatejsonstruct(current_page)
 
-    # Next & Previous Links
+    # Generate next and previous page links
     next_offset = offset + limit if current_page.has_next() else None
     prev_offset = offset - limit if offset > 0 else None  
 
@@ -120,6 +118,9 @@ def book_list(request):
 
 
 def generatejsonstruct(books):  
+    """
+    Helper function to convert book queryset into a structured JSON format.
+    """
     books_json = []
     for book in books:
         authornamedata = book.author_name
@@ -128,8 +129,9 @@ def generatejsonstruct(books):
         mimedata = book.mime_type
         urldata = book.url
 
-        author_list=[]
+        author_list = []
         
+        # Convert author data into list of dictionaries
         for name, birth, death in zip_longest(authornamedata, birthdate, deathdate, fillvalue=None):
             author_list.append({
                 "name": name,
@@ -137,27 +139,29 @@ def generatejsonstruct(books):
                 "death_year": death
             })
         
-        formatlist =[]
+        # Convert format data into list of dictionaries
+        formatlist = []
         for k in range(len(mimedata)): 
-            cache_dict = {}
-            cache_dict[mimedata[k]]= urldata[k]
-            formatlist.append(cache_dict)
+            formatlist.append({mimedata[k]: urldata[k]})
         
         books_json.append({
             "booktitle": book.booktitle,
             "authors": author_list,
-            "subjects": [sub for sub in book.subjects],
-            "bookshelves": [bookshelve for bookshelve in book.bookshelves],
-            "languages": [lang for lang in book.languages],
+            "subjects": list(book.subjects),  # Convert to list
+            "bookshelves": list(book.bookshelves),
+            "languages": list(book.languages),
             "formats": formatlist,
         })
     return books_json 
 
 
-
-def testapi(request): 
+def testapi(request):  
+    """
+    Test API to retrieve first 5 books from BooksView and return sample JSON response.
+    """
     books = BooksView.objects.all()[:5]
     books_json = []
+
     for book in books:
         authornamedata = book.author_name
         birthdate = book.birth_year
@@ -165,8 +169,9 @@ def testapi(request):
         mimedata = book.mime_type
         urldata = book.url
 
-        author_list=[]
+        author_list = []
         
+        # Convert author data into structured JSON format
         for name, birth, death in zip_longest(authornamedata, birthdate, deathdate, fillvalue=None):
             author_list.append({
                 "name": name,
@@ -174,18 +179,18 @@ def testapi(request):
                 "death_year": death
             })
         
-        formatlist =[]
+        # Convert formats into list of dictionaries
+        formatlist = []
         for k in range(len(mimedata)): 
-            cache_dict = {}
-            cache_dict[mimedata[k]]= urldata[k]
-            formatlist.append(cache_dict)
+            formatlist.append({mimedata[k]: urldata[k]})
         
         books_json.append({
             "booktitle": book.booktitle,
             "authors": author_list,
-            "subjects": [sub for sub in book.subjects],
-            "bookshelves": [bookshelve for bookshelve in book.bookshelves],
-            "languages": [lang for lang in book.languages],
+            "subjects": list(book.subjects),
+            "bookshelves": list(book.bookshelves),
+            "languages": list(book.languages),
             "formats": formatlist,
         })
+    
     return JsonResponse(books_json, safe=False)
